b944debaf21abe0eba8ba40af5ead43f8ed6caa0
Raymond Lee <raymondlee@Raymonds-iMac.local>
2020-09-03 17:18:37 -0700

    Packages

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -199,11 +199,11 @@ export default function Billing(props) {
                             <p className="pkg-price">$0</p>
                             <span className="billing-info">Free forever.</span>
          
-                                <div> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8"></i> 7 Projects</div>
+                                <p> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8;padding:5px;"></i> 7 Projects</p>
                                 <hr className="hr" />
-                                <div> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8"></i> 5 Videos per project</div>
+                                <p> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8;padding:5px;"></i> 5 Videos per project</p>
                                 <hr className="hr" />
-                                <div> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8"></i> 50MB Space for vendor uploads</div>
+                                <p> <i class="fa fa-check" aria-hidden="true" style="color:#3444D8;padding:5px;"></i> 50MB Space for vendor uploads</p>
                                 <hr className="hr" />
                           
                             <button className="btn btn-outline-custom btn-pkg-choose"
